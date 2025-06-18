d6441f500bd5203ae00a6985150632089a80a31b
Sandra-Seg <1352518483@ulife.com.br>
2025-06-18 12:55:59 -0300

    Conexao java - sql

---
+++ Cod.Java/src/DAO/bookdao.java
@@ -0,0 +1,43 @@
+package DAO;
+
+import java.sql.SQLException;
+import register.book;
+
+public class bookdao {
+
+    public void bookregister (Book book);{
+
+        String sql = "INSERT INTO BOOK (id, isbn, author_name, title, subtitle, publisher, book_subject, book_language, publication_date, page_numbers, book_genre) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+
+        PrepareStatement ps = null;
+
+        try{
+            ps = conexao.getconexao().prepareStatement(sql);
+            ps.setint(parameterIndex:1, book.getid());
+            ps.setint(parameterIndex:2, book.getisbn());
+            ps.setString(parameterIndex:3, book.getauthor_name());
+            ps.setString(parameterIndex:4, book.gettitle());
+            ps.setString(parameterIndex:5, book.getsubtitle());
+            ps.setString(parameterIndex:6, book.getpublisher());
+            ps.setString(parameterIndex:7, book.getbook_subject());
+            ps.setString(parameterIndex:8, book.getboo_language());
+            ps.setint(parameterIndex:9, book.getpublication_date());
+            ps.setint(parameterIndex:10, book.getpage_numbers());
+            ps.setString(parameterIndex:11, book.getbook_genre());
+
+            ps.execute();
+            ps.close();
+        
+
+        }catch (SQLException e){
+
+            //TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+
+
+
+    }
+
+}
---
+++ Cod.Java/src/register/book.java
@@ -0,0 +1,111 @@
+package register;
+
+public class book {
+
+    public class bookregister{
+
+        private int id;
+        private int isbn;
+        private String author_name;
+        private String title;
+        private String subtitle;
+        private String publisher;
+        private String book_subject;
+        private String book_language;
+        private int publication_date;
+        private int page_numbers;
+        private String book_genre;
+
+        public int getid(){
+            return id;
+        }
+
+        public void setid(int id){
+            this.id = id;
+        }
+
+        public int getisbn(){
+            return isbn;
+        }
+
+        public void setisbn(int isbn){
+            this.isbn = isbn;
+        }
+
+        public String getauthor_name(){
+            return author_name;
+        }
+
+        public void setauthor_name(String author_name){
+            this.author_name = author_name;
+        }
+
+        public String gettitle(){
+            return title;
+        }
+
+        public void settitle(String title){
+            this.title = title;
+        }
+
+        public String getsubtitle(){
+            return subtitletitle;
+        }
+
+        public void setsubtitle(String subtitle){
+            this.subtitle = subtitletitle;
+        }
+
+        public String getpublisher(){
+            return publisher;
+        }
+
+        public void setpublisher(String publisher){
+            this.publisher = publisher;
+        }
+
+        public String getbook_subject(){
+            return book_subject;
+        }
+
+        public void setbook_subject(String book_subject){
+            this.book_subject = book_subject;
+        }
+
+        public String book_language(){
+            return book_language;
+        }
+
+        public void setbook_language(String book_language){
+            this.book_language = book_language;
+        }
+
+        public int getpublication_date(){
+            return publication_date;
+        }
+
+        public void setpublication_date(int publication_date){
+            this.publication_date = publication_date;
+        }
+
+        public int getpage_numbers(){
+            return page_numbers;
+        }
+
+        public void setpage_numbers(int page_numbers){
+            this.page_numbers = page_nembers;
+        }
+
+        public String getbook_genre(){
+            return book_genre;
+        }
+
+        public void setbook_genre(String book_genre){
+            this.book_genre = book_genre;
+        }
+
+
+ 
+    }
+
+}
